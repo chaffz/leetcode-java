4271c61ea03f4af956f31eddd9fdbdeb27addcc8
JoshDaddy <yuxin.woo@outlook.com>
2022-11-08 18:56:09 +0800

    [updated] change to see cn.isnap.leetcode.lc75.level1.day13

--- src/main/java/cn/isnap/leetcode/DataStructure/level1/day02/TwoSum.java
+++ src/main/java/cn/isnap/leetcode/DataStructure/level1/day02/TwoSum.java
@@ -1,4 +1,7 @@
 package cn.isnap.leetcode.DataStructure.level1.day02;
 
-public class TwoSum extends cn.isnap.leetcode.lc75.level1.day13.TwoSum {
+/**
+ * @see cn.isnap.leetcode.lc75.level1.day13.TwoSum
+ */
+public class TwoSum {
 }
