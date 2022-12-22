c6647ad512d67ca431d20c340bfeb5f901399826
JoshDaddy <yuxin.woo@outlook.com>
2022-12-22 13:54:00 +0800

    [added] pskills/level1/day11: Containers & Libraries

--- src/main/java/cn/isnap/leetcode/ProgrammingSkills/level1/day11/SortIntegersByTheNumberOf1Bits.java
+++ src/main/java/cn/isnap/leetcode/ProgrammingSkills/level1/day11/SortIntegersByTheNumberOf1Bits.java
@@ -1,12 +1,11 @@
 package cn.isnap.leetcode.ProgrammingSkills.level1.day11;
 
 import java.util.*;
-import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class SortIntegersByTheNumberOf1Bits {
     public int[] sortByBits(int[] arr) {
-        List<Integer> data = Arrays.stream(arr).map(Integer::new).boxed().collect(Collectors.toList());
-        data.sort(new Comparator<Integer>() {
+        Stream<Integer> data = Arrays.stream(arr).map(Integer::new).boxed().sorted(new Comparator<Integer>() {
             @Override
             public int compare(Integer o1, Integer o2) {
                 int bit1 = Integer.bitCount(o1);
@@ -19,6 +18,6 @@ public class SortIntegersByTheNumberOf1Bits {
             }
         });
 
-        return data.stream().mapToInt(Integer::intValue).toArray();
+        return data.mapToInt(Integer::intValue).toArray();
     }
 }
