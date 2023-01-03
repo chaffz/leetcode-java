ae6fcac4fb60f8955d6aa081dc723378eaf50307
JoshDaddy <yuxin.woo@outlook.com>
2023-01-03 20:28:19 +0800

    [added] pskills/level2/day11

--- src/main/java/cn/isnap/leetcode/ProgrammingSkills/level2/day11/GroupAnagrams.java
+++ src/main/java/cn/isnap/leetcode/ProgrammingSkills/level2/day11/GroupAnagrams.java
@@ -2,27 +2,21 @@ package cn.isnap.leetcode.ProgrammingSkills.level2.day11;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.stream.Collectors;
 
 public class GroupAnagrams {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> caches = new ConcurrentHashMap<>();
-        Arrays.stream(strs).parallel().forEach(s -> {
-            encodeAnagram(caches, s);
-        });
-
+        Map<String, List<String>> caches = new HashMap<>();
+        for (String str : strs) {
+            encodeAnagram(caches, str);
+        }
         return new ArrayList<>(caches.values());
     }
 
     private void encodeAnagram(Map<String, List<String>> caches, String in) {
-        int[] counter = new int[26];
         char[] chars = in.toCharArray();
-        for (char aChar : chars) {
-            counter[aChar - 'a']++;
-        }
-        String key = Arrays.toString(counter);
-        List<String> values = caches.getOrDefault(key, new ArrayList<>());
-        values.add(in);
-        caches.put(key, values);
+        Arrays.sort(chars);
+        String key = new String(chars);
+        caches.putIfAbsent(key, new ArrayList<>());
+        caches.get(key).add(in);
     }
 }
