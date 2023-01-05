0b2674ffb2a2890518a11ace3e926e61ecc69f45
JoshDaddy <yuxin.woo@outlook.com>
2023-01-05 19:36:16 +0800

    [added] pskills/level2/day13

---
+++ src/main/java/cn/isnap/leetcode/ProgrammingSkills/level2/day13/SmallestRangeII.java
@@ -0,0 +1,18 @@
+package cn.isnap.leetcode.ProgrammingSkills.level2.day13;
+
+import java.util.Arrays;
+
+public class SmallestRangeII {
+    public int smallestRangeII(int[] nums, int k) {
+        Arrays.sort(nums);
+        int ans = nums[nums.length - 1] - nums[0];
+        for (int i = 0; i < nums.length - 1; i++) {
+            int a = nums[i], b = nums[i + 1];
+            int hi = Math.max(nums[nums.length - 1] - k, a + k);
+            int lo = Math.min(nums[0] + k, b - k);
+            ans = Math.min(ans, hi - lo);
+        }
+
+        return ans;
+    }
+}
