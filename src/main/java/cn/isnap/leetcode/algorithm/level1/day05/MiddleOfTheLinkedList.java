5a6ff5fad2bf94961290946e9b01e9bd7883ada4
JoshDaddy <yuxin.woo@outlook.com>
2023-01-17 08:22:56 +0800

    [added] alg/level1/day05

---
+++ src/main/java/cn/isnap/leetcode/algorithm/level1/day05/MiddleOfTheLinkedList.java
@@ -0,0 +1,7 @@
+package cn.isnap.leetcode.algorithm.level1.day05;
+
+/**
+ * @see cn.isnap.leetcode.lc75.level1.day04.MiddleOfTheLinkedList
+ */
+public class MiddleOfTheLinkedList {
+}
---
+++ src/main/java/cn/isnap/leetcode/algorithm/level1/day05/RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,7 @@
+package cn.isnap.leetcode.algorithm.level1.day05;
+
+/**
+ * @see cn.isnap.leetcode.lc75.level2.day03.RemoveNthNodeFromEndOfList
+ */
+public class RemoveNthNodeFromEndOfList {
+}
