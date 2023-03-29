2eef383451fa33fd95d7054819bd06959685b372
JoshDaddy <yuxin.woo@outlook.com>
2023-03-29 20:14:50 +0800

    [added] backtrack

--- src/main/java/cn/isnap/leetcode/backtrack/ClosestDessertCost.java
+++ src/main/java/cn/isnap/leetcode/backtrack/ClosestDessertCost.java
@@ -1,16 +1,18 @@
 package cn.isnap.leetcode.backtrack;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class ClosestDessertCost {
+  int closest = Integer.MAX_VALUE, diff = Integer.MAX_VALUE, abs;
   public int closestCost(int[] baseCosts, int[] toppingCosts, int target) {
-    int closest = Integer.MAX_VALUE, diff = Integer.MAX_VALUE, abs;
-    Set<Integer> costs = new HashSet<>();
+
     for (final int baseCost : baseCosts) {
-      closestCost(costs, baseCost, toppingCosts, 0);
+      closestCost(baseCost, target, toppingCosts, 0);
     }
-    for (final Integer cost : costs) {
+
+    return closest;
+  }
+
+  private void closestCost(int cost, int target, int[] toppingCosts, int index) {
+    if (index >= toppingCosts.length) {
       abs = Math.abs(cost - target);
       if (abs < diff) {
         diff = abs;
@@ -18,19 +20,12 @@ public class ClosestDessertCost {
       } else if (abs == diff) {
         closest = Math.min(closest, cost);
       }
-    }
-    return closest;
-  }
-
-  private void closestCost(Set<Integer> costs, int base, int[] toppingCosts, int index) {
-    if (index >= toppingCosts.length) {
-      costs.add(base);
       return;
     }
     int topping;
     for (int i = 0; i <= 2; i++) {
-      topping = toppingCosts[index] * i + base;
-      closestCost(costs, topping, toppingCosts, index + 1);
+      topping = toppingCosts[index] * i + cost;
+      closestCost(topping, target, toppingCosts, index + 1);
     }
   }
 }
